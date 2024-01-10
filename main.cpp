570ea87ae6d56fe4978d19f39a4569addf1393f7
kc++ <2745296803@qq.com>
2024-01-10 20:44:50 +0800

    1

--- main.cpp
+++ main.cpp
@@ -1,8 +1,56 @@
 #include <iostream>
 #include <stack>
+
 using namespace std;
 
-int forLoop(int n)
+int fac(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+    return n * fac(n - 1);
+}
+
+int line (float n)
+{
+    if (n <= 1)
+    {
+        return 1;
+    }
+    int count = line(n / 2) + line(n / 2);
+    for (int i = 0; i < (int) n + 1; i++)
+    {
+        count++;
+    }
+    return count;
+}
+
+int log (float n)
+{
+    if (n <= 1)
+    {
+        return 0;
+    }
+    return log(n / 2) + 1;
+}
+
+int exp (int n)
+{
+    int count = 0;
+    int base = 1;
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < base; ++j)
+        {
+            count++;
+        }
+        base *= 2;
+    }
+    return count;
+}
+
+int forLoop (int n)
 {
     stack<int> sta;
     int res = 0;
@@ -18,10 +66,9 @@ int forLoop(int n)
     return res;
 }
 
+
 int main ()
 {
-    int n = 0;
-    cin >> n;
-    cout << forLoop(n) << endl;
+    cout << fac(5) << endl;
     return 0;
 }
