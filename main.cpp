cd8db2f3f2959138d61607fa3c9008e81dff95df
kc++ <2745296803@qq.com>
2024-01-10 15:50:17 +0800

    1

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include <stack>
+using namespace std;
+
+int forLoop(int n)
+{
+    stack<int> sta;
+    int res = 0;
+    for (int i = n; i > 0; i--)
+    {
+        sta.push(i);
+    }
+    while (!sta.empty())
+    {
+        res += sta.top();
+        sta.pop();
+    }
+    return res;
+}
 
 int main ()
 {
-    std::cout << "Hello, World!" << std::endl;
+    int n = 0;
+    cin >> n;
+    cout << forLoop(n) << endl;
     return 0;
 }
