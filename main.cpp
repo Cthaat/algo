b6d9403ef20764eef0340bedd8a6b753ccc84a12
kc++ <2745296803@qq.com>
2024-03-01 23:16:58 +0800

    sort

--- main.cpp
+++ main.cpp
@@ -2338,9 +2338,27 @@ int minCostClimbingStairsDP (vector<int> &cost)
     return dp[n];
 }
 
+int climbingStairsConstraintDP(int n)
+{
+    if (n == 1 || n == 2)
+    {
+        return 1;
+    }
+    vector<vector<int>> dp (n + 1, vector<int>(3, 0));
+    dp[1][1] = 1;
+    dp[1][2] = 0;
+    dp[2][1] = 0;
+    dp[2][2] = 1;
+    for (int i = 3 ; i <= n ; ++i)
+    {
+        dp[i][1] = dp[i - 1][2];
+        dp[i][2] = dp[i - 2][1] + dp[i - 2][2];
+    }
+    return dp[n][1] + dp[n][2];
+}
+
 int main ()
 {
-    vector<int> cost = {0, 15, 2 , 1 , 5 , 4 , 3 , 2 , 1 , 1 , 0};
-    cout << minCostClimbingStairsDP(cost) << endl;
+    std::cout << climbingStairsConstraintDP(10) << std::endl;
     return 0;
 }
